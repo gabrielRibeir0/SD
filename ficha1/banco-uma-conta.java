af7347a5ae9eb34dbedaf1257055643ec5e221c3
Gabriel Ribeiro <a104171@alunos.uminho.pt>
2024-10-04 15:20:44 +0100

    início Ficha 3

--- ficha1/banco-uma-conta.java
+++ ficha1/banco-uma-conta.java
@@ -1,7 +1,7 @@
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-class Bank{
+class BankUmaConta{
   private static class Account {
     private int balance;
 
